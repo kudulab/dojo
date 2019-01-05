a49a941f4b6e8d50dff853cfe0ac60756a75c8d6
Ewa Czechowska <ewa@ai-traders.com>
2019-01-05 21:20:29 +0100

    poc - run docker container interactively

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
+func RunShellInteractive(cmdString string) {
+	cmd := exec.Command("bash", "-c", cmdString)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	println(err)
+
+	exitStatus := 0
+	println(exitStatus)
+}
+
 func main() {
 	println("hello in dojo")
+	cmdString := "docker run -ti alpine:3.8"
+	RunShellInteractive(cmdString)
 }
