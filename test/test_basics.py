c2115fb3ad68210fadd06201ea02bb40f889a153
xmik <ewa@kudulab.io>
2022-02-27 14:01:32 +0000

    move e2e_invalid_environment to run under other e2e tests

--- test/test_basics.py
+++ test/test_basics.py
@@ -1,5 +1,5 @@
 import pytest
-from .support.common import run_dojo
+from .support.common import run_dojo, run_command, get_dojo_exe
 
 
 def test_version_output():
@@ -15,3 +15,11 @@ def test_help_output():
     assert 'Usage of dojo' in result.stderr, dojo_combined_output_str
     assert 'Driver: docker or docker-compose' in result.stderr, dojo_combined_output_str
     assert result.returncode == 0
+
+
+def test_dojo_prints_error_if_bash_not_installed():
+    dojo_exe = get_dojo_exe()
+    result = run_command('docker', ['run', '-t', '--rm', '-v', f"{dojo_exe}:/usr/bin/dojo", "alpine:3.15", "/usr/bin/dojo"])
+    combined_output_str =  "stdout:\n{0}\nstderror:\n{1}".format(result.stdout, result.stderr)
+    assert 'Error while verifying if Bash is installed' in result.stdout, combined_output_str
+    assert result.returncode == 1
